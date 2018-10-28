9408b20392bcaca494e4c23745c01295b90f00c1
stone.wangbo <stone.wangbo@gmail.com>
2018-10-28 15:30:29 +0800

    多种实现

--- src/main/java/com/leetcode/easy/TwoSum.java
+++ src/main/java/com/leetcode/easy/TwoSum.java
@@ -1,9 +1,12 @@
 package com.leetcode.easy;
 
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    //循环实现
+    public int[] twoSum1(int[] nums, int target) {
 
         for(int a=0;a<nums.length-1;a++){
             for(int b=1;b<nums.length;b++){
@@ -16,6 +19,20 @@ public class TwoSum {
 
             }
 
+        }
+
+        return null;
+    }
+
+    //第二种，快速实现
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int a=0;a<nums.length;a++){
+            int need = target-nums[a];
+            if(map.containsKey(need)){
+                return  new int[]{map.get(need),a};
+            }
+            map.put(nums[a],a);
         }
 
         return null;
--- src/test/java/com/leetcode/easy/TwoSumTest.java
+++ src/test/java/com/leetcode/easy/TwoSumTest.java
@@ -5,15 +5,28 @@ import org.junit.Test;
 
 public class TwoSumTest {
     @Test
-    public void test(){
+    public void test1(){
         TwoSum test = new TwoSum();
-        int[] res =  test.twoSum(new int[]{2,7,11,15},9);
+        int[] res =  test.twoSum1(new int[]{2,7,11,15},9);
         Assert.assertArrayEquals(res,new int[]{0,1});
 
-         res =  test.twoSum(new int[]{3,2,3},6);
+         res =  test.twoSum1(new int[]{3,2,3},6);
         Assert.assertArrayEquals(res,new int[]{0,2});
 
-        res =  test.twoSum(new int[]{3,2,4},6);
+        res =  test.twoSum1(new int[]{3,2,4},6);
+        Assert.assertArrayEquals(res,new int[]{1,2});
+    }
+
+    @Test
+    public void test2(){
+        TwoSum test = new TwoSum();
+        int[] res =  test.twoSum2(new int[]{2,7,11,15},9);
+        Assert.assertArrayEquals(res,new int[]{0,1});
+
+        res =  test.twoSum2(new int[]{3,2,3},6);
+        Assert.assertArrayEquals(res,new int[]{0,2});
+
+        res =  test.twoSum2(new int[]{3,2,4},6);
         Assert.assertArrayEquals(res,new int[]{1,2});
     }
 }
